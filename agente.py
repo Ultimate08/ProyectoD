154b222fdc3c5f4a6a0c6046c5f537e1763a17db
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-11-04 20:30:02 -0600

    Add files via upload

---
+++ agente.py
@@ -0,0 +1,60 @@
+from collections import namedtuple
+import socket
+import threading
+import datetime
+
+# Configura el servidor
+nodos = [
+    {'host': '192.168.229.129', 'port': 12345},
+    {'host': '192.168.229.130', 'port': 12346},
+    {'host': '192.168.229.131', 'port': 12347},
+    {'host': '192.168.229.132', 'port': 12348}
+]
+
+b = open("/home/eduardo/SD/bitacora.txt", "x")
+
+def server(n):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((n['host'], n['port']))
+        server_socket.listen()
+        print(f"Servidor escuchando en {n['host']}:{n['port']}")
+        conn, addr = server_socket.accept()
+
+        with conn:
+            print(f"Conexión establecida desde {addr}")
+
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f"Mensaje recibido del cliente: {data.decode('utf-8')}")
+                response = "El mensaje ha sido recibido\n"
+                conn.sendall(response.encode('utf-8'))
+
+def client(n):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((n['host'], n['port']))
+        while True:
+            message = input("Escribe un mensaje para el servidor: ")
+            tmp = datetime.datetime.now()
+            msg = message+" "+tmp
+            client_socket.sendall(msg.encode('utf-8'))
+            data = client_socket.recv(1024)
+            b.write("\n"+msg)
+            print(f"Respuesta del servidor: {data.decode('utf-8')}")
+
+print("Elige un servidor:")
+for i, n in enumerate(nodos):
+    print(f"{i + 1}. {n['host']}:{n['port']}")
+
+choice = int(input("Selecciona el número del servidor: ")) - 1
+
+# Crear y ejecutar hilos para el servidor y el cliente
+server_thread = threading.Thread(target=server, args=(nodos[choice],))
+client_thread = threading.Thread(target=client, args=(nodos[choice],))
+
+server_thread.start()
+client_thread.start()
+
+server_thread.join()
+client_thread.join()
