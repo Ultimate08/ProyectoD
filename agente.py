fefffc1d3983bda45b3f1b172f00ea0c7dd784c2
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-11-10 15:54:36 -0600

    Update agente.py

--- agente.py
+++ agente.py
@@ -5,10 +5,10 @@ import datetime
 
 # Configura las IP de tus nodos
 nodos = [
-    {'host': '192.168.153.128', 'port': 1111},
-    {'host': '192.168.153.129', 'port': 2222},
-    {'host': '192.168.153.130', 'port': 3333},
-    {'host': '192.168.153.131', 'port': 4444}
+    '192.168.153.128',
+    '192.168.153.129',
+    '192.168.153.130',
+    '192.168.153.131'
 ]
 
 b = open("/home/eduardo/SD/bitacora.txt", "w") # Cambien la ruta y creen el archivo
@@ -17,14 +17,9 @@ def server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         hn = socket.getfqdn()
         ip = socket.gethostbyname(hn)
-        port = 0 
-        for b in nodos:
-            if (ip == b['host']):
-                port = b['port']
-                break
-        server_socket.bind((ip, port))
+        server_socket.bind((ip, 5555))
         server_socket.listen()
-        print(f"Servidor escuchando en {ip}:{port}")
+        print(f"Servidor escuchando en {ip}:{5555}")
         conn, addr = server_socket.accept()
 
         with conn:
@@ -41,7 +36,7 @@ def server():
 
 def client(ip, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((ip, port))
+        client_socket.connect((ip, 5555))
         if key.lower() == 'e':
             message = input("Escribe un mensaje para el servidor: ")
             tmp = str(datetime.datetime.now())
@@ -56,10 +51,9 @@ for i, n in enumerate(nodos):
     print(f"{i + 1}. {n['host']}:{n['port']}")
 
 choice = int(input("Selecciona el número del servidor: ")) - 1
-c = nodos[choice]
 # Crear y ejecutar hilos para el servidor y el cliente
 server_thread = threading.Thread(target=server)
-client_thread = threading.Thread(target=client, args=(c['host'],c['port']))
+client_thread = threading.Thread(target=client, args=(nodos[choice]))
 
 server_thread.start()
 client_thread.start()
