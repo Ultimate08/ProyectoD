587aec2116889f2d187cc6a633e7f1526facbe38
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-11-10 16:39:31 -0600

    Update agente.py

--- agente.py
+++ agente.py
@@ -54,7 +54,7 @@ for i,j in enumerate(nodos):
 choice = int(input("Selecciona el número del servidor: ")) - 1
 # Crear y ejecutar hilos para el servidor y el cliente
 server_thread = threading.Thread(target=server)
-client_thread = threading.Thread(target=client, args=(nodos[choice],))
+client_thread = threading.Thread(target=client, args=('127.0.1.1',))
 
 server_thread.start()
 client_thread.start()
