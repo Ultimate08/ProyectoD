4a8f7d566490e2ffb5c1781994dd3d77820897dd
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-02 14:15:26 -0600

    Add files via upload

--- servidor.py
+++ servidor.py
@@ -22,18 +22,18 @@ class servidor():
         cliente_sockets = []
         hilos = []
         socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket_servidor.bind(('192.168.153.128', 1111))
-        socket_servidor.listen(4) #Permite escuchar 3 conexiones
-        conn, addr = socket_servidor.accept()
-        with conn:
-            print("\nSocket creado, escuchando desde ",addr)
-            while True:
-                #metodo para que acepte conexiones
-                conexion_socket, direccion = socket_servidor.accept()
-                print("Primera conexion desde: ", direccion[0])
-                hilo = hilo_servidor(conexion_socket, direccion, cliente_sockets)
-                hilo.start()
-                hilos.append(hilo)
-                cliente_sockets.append(conexion_socket)
+        socket_servidor.bind(('0.0.0.0', 8888))
+        socket_servidor.listen(3) #Permite escuchar 3 conexiones
+        print("\nSocket creado, escuchando desde [192.168.74.222] y en el puerto [8888]")
 
-servidor.iniciar()
+
+        while True:
+            #metodo para que acepte conexiones
+            conexion_socket, direccion = socket_servidor.accept()
+            print("Primera conexion desde: ", direccion[0])
+            hilo = hilo_servidor(conexion_socket, direccion, cliente_sockets)
+            hilo.start()
+            hilos.append(hilo)
+            cliente_sockets.append(conexion_socket)
+
+# servidor.iniciar()
