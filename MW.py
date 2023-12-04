3754bd9426bf25e6095a39d1be5d549593a6bc75
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-04 09:23:37 -0600

    Update MW.py

--- MW.py
+++ MW.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
 
         while espera:
             # Espera por datos
-            peticion = socket_cliente.recv(1024)
+            peticion = servidor.recv(1024)
          
             # Si recibimos cero bytes, es que el cliente ha cerrado el socket
             if not peticion:
@@ -175,14 +175,13 @@ if __name__ == "__main__":
             # Contestacion a "maestro"
             if ("maestro" in peticion.decode()):
                 print (str(datos_cliente)+ " envia hola: contesto")
-                socket_cliente.send("pues hola".encode())
+               servidor.send("pues hola".encode())
              
             # Contestacion y cierre a "cliente"
             if ("cliente" in peticion.decode()):
-                nom = socket_cliente.recv(1024)
-                apPat = socket_cliente.recv(1024)
-                apMat = socket_cliente.recv(1024)
-                socket_cliente.send("pues adios".encode())
+                nom = servidor.recv(1024)
+                apPat = servidor.recv(1024)
+                apMat = servidor.recv(1024)
                 cur.execute('INSERT INTO CLIENTES (idCliente, nombre, apPaterno, apMaterno) VALUES (?,?,?,?)',(idC,nom,apPat,apMat))
                 idC += 1
                 bd.commit()
