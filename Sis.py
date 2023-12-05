ac9f1315dde071a8b227e171abe602df6a59c8a3
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-05 12:31:37 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -107,7 +107,8 @@ if __name__ == "__main__":
                 print("(idSucursal, producto, cantidad)")
                 for fila in cur:
                     print(fila)
-                a = cur.execute('SELECT total FROM PRODUCTOS WHERE idProducto = 1')
-                print(a)
+                cur.execute('SELECT total FROM PRODUCTOS WHERE idProducto = 1')
+                r = cur.fetchone()
+                print(r)
         except ValueError:
             print("Entrada inválida. Ingrese un número válido o '0' para salir.")
