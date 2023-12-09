2e4f20fa526540a82c7990999f613ffbd3aab588
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-08 19:49:02 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -60,10 +60,11 @@ if __name__ == "__main__":
                 n = input("\nCuál es el nombre del cliente?: ")
                 p = input("\nCuál es el apellido paterno del cliente?: ")
                 m = input("\nCuál es el apellido materno del cliente?: ")
-                #MWf.mensaje(hosts[0],port[0])
-                MWf.mensaje(hosts[1],port[1])
-                MWf.mensaje(hosts[2],port[2])
-                MWf.mensaje(hosts[3],port[3])
+                msj = n+" "+p+" "+m
+                #MWf.mensaje(hosts[0],port[0],msj)
+                MWf.mensaje(hosts[1],port[1],msj)
+                MWf.mensaje(hosts[2],port[2],msj)
+                MWf.mensaje(hosts[3],port[3],msj)
         
             elif choice == '3':
                print("")
