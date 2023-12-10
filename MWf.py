bfa3fdda61b392e4cea6448937ee945b5e1f22f7
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-10 15:28:40 -0600

    Update MWf.py

--- MWf.py
+++ MWf.py
@@ -84,7 +84,7 @@ def cliente(conn, addr):
 
 def servidor(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
+        #s.bind((host, port))
         s.listen(5)
         print(f"Servidor escuchando en {host}:{port}")
 
