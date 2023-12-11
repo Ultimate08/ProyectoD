e185a60a783fe385fe27b6aed34efd3556ce7f90
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-10 18:25:59 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -24,18 +24,14 @@ if __name__ == "__main__":
     ]
 
     # Iniciar los servidores en cada máquina virtual
-    if (ipl == hosts[0]):
-        vm1 = threading.Thread(target=MWf.servidor, args=(hosts[0], port[0]))
-        vm1.start()
-    elif (ipl == hosts[1]):
-        vm2 = threading.Thread(target=MWf.servidor, args=(hosts[1], port[1]))
-        vm2.start()
-    elif (ipl == hosts[2]):
-        vm3 = threading.Thread(target=MWf.servidor, args=(hosts[2], port[2]))
-        vm3.start()
-    elif (ipl == hosts[3]):
-        vm4 = threading.Thread(target=MWf.servidor, args=(hosts[3], port[3]))
-        vm4.start()
+    vm1 = threading.Thread(target=MWf.servidor, args=(hosts[0], port[0]))
+    vm1.start()
+    vm2 = threading.Thread(target=MWf.servidor, args=(hosts[1], port[1]))
+    vm2.start()
+    vm3 = threading.Thread(target=MWf.servidor, args=(hosts[2], port[2]))
+    vm3.start()
+    vm4 = threading.Thread(target=MWf.servidor, args=(hosts[3], port[3]))
+    vm4.start()
     
     while True:
         # Menu de seleccion
