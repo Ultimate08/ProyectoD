f70b9521e33179f704c38dcf62937816d62680e9
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-10 21:09:31 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -21,20 +21,31 @@ if __name__ == "__main__":
         3333,
         4444
     ]
+    
+    names = [    # Identificadores de las máquinas
+    "56 4d b8 d4 59 5c 11 53-09 55 ae 8a 8f ff 51 50",
+    "56 4d dc 63 62 9c cb 1c-13 45 ad 0f 02 0f df 22",
+    "56 4d 94 6e e2 81 de 60-8e eb 92 b6 22 ca 2c 9a",
+    "56 4d a3 80 4a 63 88 e9-63 94 0e eb 5e af 4c 5c"
+    ]
 
     # Iniciar los servidores en cada máquina virtual
-    vm1 = threading.Thread(target=MWf.servidor, args=(hosts[0], port[0]))
-    vm1.start()
-    vm2 = threading.Thread(target=MWf.servidor, args=(hosts[1], port[1]))
-    vm2.start()
-    vm3 = threading.Thread(target=MWf.servidor, args=(hosts[2], port[2]))
-    vm3.start()
-    vm4 = threading.Thread(target=MWf.servidor, args=(hosts[3], port[3]))
-    vm4.start()
+    uuid = MWf.obtener_uuid()
+    if (uuid == names[0]):
+        vm1 = threading.Thread(target=MWf.servidor, args=(hosts[0], port[0]))
+        vm1.start()
+    elif (uuid == names[1]):
+        vm2 = threading.Thread(target=MWf.servidor, args=(hosts[1], port[1]))
+        vm2.start()
+    elif (uuid == names[2]):
+        vm3 = threading.Thread(target=MWf.servidor, args=(hosts[2], port[2]))
+        vm3.start()
+    elif (uuid == names[3]):
+        vm4 = threading.Thread(target=MWf.servidor, args=(hosts[3], port[3]))
+        vm4.start()
     
     while True:
         # Menu de seleccion
-        #hn = socket.gethostbyname(socket.gethostname())
         print("\nBienvenido al sistema de inventarios, que deseas hacer?:")
         print("\n1. Consultar clientes")
         print("\n2. Agregar nuevo cliente")
