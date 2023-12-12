b1255d05d6f903ed693da55ea897f5343b1394cc
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-11 22:05:01 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -46,7 +46,6 @@ if __name__ == "__main__":
     
     while True:
         # Menu de seleccion
-        print(hn)
         print("\nBienvenido al sistema de inventarios, que deseas hacer?:")
         print("\n1. Consultar clientes")
         print("\n2. Agregar nuevo cliente")
