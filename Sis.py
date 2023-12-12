810e1f0bc6304cb25e91848abc3191046b24439f
Eduardo F <42392930+Ultimate08@users.noreply.github.com>
2023-12-11 21:53:25 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -8,8 +8,6 @@ bd = sqlite3.connect('/home/eduardo/base.sqlite', check_same_thread=False)
 cur = bd.cursor()
 
 if __name__ == "__main__":
-
-    cur.execute('CREATE TABLE INVENTARIO (idSucursal, producto INTEGER, cantidad INTEGER)')
     # Configuración de los servidores en cada máquina virtual
     hosts = [
         "192.168.153.128",
